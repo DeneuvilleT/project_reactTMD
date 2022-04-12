08d7902658715c5b58c5bf9a1c2cd80282fef5f1
DeneuvilleT <deneuville.thomas@gmail.com>
2022-04-12 18:42:48 +0200

    Final Version

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,66 +1,64 @@
 import React, { useEffect, useState } from 'react';
-import { getCat } from '../api/axios';
+import { getData as getData } from '../api/axios';
+import styles from '../Containers/home.module.css';
+import '../App.css'
 
-function Home(props) {
+function Home() {
 
-    const [name, setName] = useState("ro");
-    const [arr, setArr] = useState([]);
-    const [msg, setMsg] = useState(null)
-    const [inputs, setInputs] = useState({
-        name: "",
-        age: "",
-    });
+    const [myInputValue, setInputs] = useState("");
+    const [myResults, setResults] = useState([]);
 
     useEffect(() => {
-        axiosRequest();
-        updateState();
-        // fonction de nettoyage / cleanup function
+        axiosRequest(myInputValue);
         return () => {
-            // console.log("cleanup");
-            // clearTimeout(timer)
-        }
-    }, [])
+        };
+    }, [myInputValue]);
 
-    const updateState = () => {
-        // console.log("useEffect");
-        setName("jaky");
-    }
 
-    const axiosRequest = () => {
-        getCat().then(res => {
+    const axiosRequest = (myInputValue) => {
+        getData(myInputValue).then(res => {
             if (res.status === 200) {
-                setArr(res.data)
-                console.log(res);
+                console.log(res)
+                setResults(res.data.results)
             } else {
                 console.log('err', res);
-                setMsg("une erreur reseau est survenue !! contactez l'admin !!");
-            }
-        })
-    }
+            };
+        });
+    };
+
 
     return (
         <>
-            {console.log('render', arr)}
-            <h1>Welcome to my home !!</h1>
+            <h1>Recherche de Film</h1>
+            <input
+                type="text"
+                onChange={(e) => setInputs(e.target.value)}
+            />
 
-            <p>my name is : {name}</p>
-            <p>{props.age} yo</p>
+            <div id='results'>
+                {
+                    myResults.map((elem) => {
+                        return (
+                            <React.Fragment key={elem.id}>
+                                <div className={styles.home}>
+                                    <h3 href={elem.id} >{elem.title}</h3>
+                                    <img src={
+                                        elem.poster_path === null ? 'https://media.istockphoto.com/photos/bowl-of-popcorn-in-front-of-a-cinema-projector-picture-id182790916?k=6&m=182790916&s=170667a&w=0&h=0L_xlkRTQnNG0Ks0QiovYlk9YkScyDEn-YRRlcNFCS4=' :
+                                            `https://image.tmdb.org/t/p/w300${elem.poster_path}`}
+                                        alt={elem.title} />
+                                    <details style={{ textDecoration: 'underline', cursor: 'pointer' }}>{elem.overview}</details>
+                                    <p style={{ fontWeight: 'bolder' }}>{elem.original_language.toUpperCase()}</p>
+                                    <p>{elem.release_date}</p>
+                                    <p>{elem.vote_average} / 10</p>
+                                </div>
 
-            {
-                arr.length && arr.map(cat => {
-                    return (
-                        <React.Fragment key={cat.id}>
-                            <img src={cat.url} />
-                        </React.Fragment>
-                    )
-                })
-            }
-
-            {
-                msg !== null && <p>{msg}</p>
-            }
+                            </React.Fragment>
+                        )
+                    })
+                }
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export function getCat() {
+export function getData(keyWord) {
 
-    return axios.get("https://api.thecatapi.com/v1/images/search")
+    return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=acc7fb5c99e94de089fe3049f362676b&language=en-US&query=${keyWord}&page=1&include_adult=false`)
         .then(res => {
             return res
         }).catch(err => {
