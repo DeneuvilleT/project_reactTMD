351c97db46e06aae8317f7c3ca184b55ebcc8096
DeneuvilleT <deneuville.thomas@gmail.com>
2022-04-13 15:45:47 +0200

    13/04/20224

---
+++ src/Containers/Cards/Cards.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../Cards/home.module.css';
+
+export function Cards(props) {
+
+    return (
+        <>
+            <React.Fragment key={props.dataElem.id}>
+                <div className={styles.home}>
+                    <h3 >{props.dataElem.title}</h3>
+                    <img src={
+                        props.dataElem.poster_path === null ? 'https://media.istockphoto.com/photos/bowl-of-popcorn-in-front-of-a-cinema-projector-picture-id182790916?k=6&m=182790916&s=170667a&w=0&h=0L_xlkRTQnNG0Ks0QiovYlk9YkScyDEn-YRRlcNFCS4=' :
+                            `https://image.tmdb.org/t/p/w300${props.dataElem.poster_path}`}
+                        alt={props.dataElem.title} />
+                    <details style={{ textDecoration: 'underline', cursor: 'pointer' }}>{props.dataElem.overview}</details>
+                    <p style={{ fontWeight: 'bolder' }}>Version Originale : {props.dataElem.original_language.toUpperCase()}</p>
+                    <p>{props.dataElem.release_date}</p>
+                    <p style={{ fontWeight: 'bolder' }} >{props.dataElem.vote_average} / 10</p>
+                </div>
+            </React.Fragment>
+        </>
+    );
+};
--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getData as getData } from '../api/axios';
-import styles from '../Containers/home.module.css';
+import { Cards } from './Cards/Cards';
 import '../App.css'
 
 function Home() {
@@ -10,15 +10,12 @@ function Home() {
 
     useEffect(() => {
         axiosRequest(myInputValue);
-        return () => {
-        };
     }, [myInputValue]);
 
 
     const axiosRequest = (myInputValue) => {
         getData(myInputValue).then(res => {
             if (res.status === 200) {
-                console.log(res)
                 setResults(res.data.results)
             } else {
                 console.log('err', res);
@@ -26,34 +23,20 @@ function Home() {
         });
     };
 
-
     return (
         <>
             <h1>Recherche de Film</h1>
-            <input
-                type="text"
-                onChange={(e) => setInputs(e.target.value)}
-            />
+            <hr />
+            <input type="text" onChange={(e) => setInputs(e.target.value)} />
 
             <div id='results'>
                 {
                     myResults.map((elem) => {
                         return (
-                            <React.Fragment key={elem.id}>
-                                <div className={styles.home}>
-                                    <h3 href={elem.id} >{elem.title}</h3>
-                                    <img src={
-                                        elem.poster_path === null ? 'https://media.istockphoto.com/photos/bowl-of-popcorn-in-front-of-a-cinema-projector-picture-id182790916?k=6&m=182790916&s=170667a&w=0&h=0L_xlkRTQnNG0Ks0QiovYlk9YkScyDEn-YRRlcNFCS4=' :
-                                            `https://image.tmdb.org/t/p/w300${elem.poster_path}`}
-                                        alt={elem.title} />
-                                    <details style={{ textDecoration: 'underline', cursor: 'pointer' }}>{elem.overview}</details>
-                                    <p style={{ fontWeight: 'bolder' }}>{elem.original_language.toUpperCase()}</p>
-                                    <p>{elem.release_date}</p>
-                                    <p>{elem.vote_average} / 10</p>
-                                </div>
-
-                            </React.Fragment>
-                        )
+                            <>
+                                <Cards dataElem={elem} />
+                            </>
+                        );
                     })
                 }
             </div>
